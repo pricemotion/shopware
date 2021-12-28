7203f42571bf8a02319385b2d82e14942586ea81
Albert Peschar <albert@peschar.net>
2021-12-28 10:13:10 +0200

    Run make format

--- plugin/src/Resources/app/administration/src/main.js
+++ plugin/src/Resources/app/administration/src/main.js
@@ -1,18 +1,18 @@
-import './page/sw-product-detail'
-import './view/sw-product-detail-pricemotion'
+import "./page/sw-product-detail";
+import "./view/sw-product-detail-pricemotion";
 
-Shopware.Module.register('kibo-pricemotion', {
-    routeMiddleware(next, currentRoute) {
-        if (currentRoute.name === 'sw.product.detail') {
-            currentRoute.children.push({
-                name: 'sw.product.detail.pricemotion',
-                path: '/sw/product/detail/:id/pricemotion',
-                component: 'sw-product-detail-pricemotion',
-                meta: {
-                    parentPath: 'sw.product.index'
-                }
-            })
-        }
-        next(currentRoute)
+Shopware.Module.register("kibo-pricemotion", {
+  routeMiddleware(next, currentRoute) {
+    if (currentRoute.name === "sw.product.detail") {
+      currentRoute.children.push({
+        name: "sw.product.detail.pricemotion",
+        path: "/sw/product/detail/:id/pricemotion",
+        component: "sw-product-detail-pricemotion",
+        meta: {
+          parentPath: "sw.product.index",
+        },
+      });
     }
-})
+    next(currentRoute);
+  },
+});
