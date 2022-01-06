f7307445e444d7fbf240595d252bba7bc27c455b
Albert Peschar <albert@peschar.net>
2022-01-06 12:03:05 +0200

    phan: Add vendor directories

--- .phan/config.php
+++ .phan/config.php
@@ -4,8 +4,10 @@ $vendor = [
     'shopware/vendor/doctrine/dbal/lib',
     'shopware/vendor/shopware/core',
     'shopware/vendor/symfony/console',
+    'shopware/vendor/symfony/event-dispatcher',
     'shopware/vendor/symfony/framework-bundle',
     'shopware/vendor/symfony/http-foundation',
+    'shopware/vendor/symfony/messenger',
     'shopware/vendor/symfony/routing',
 ];
 
