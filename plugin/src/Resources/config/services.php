24933975ee175d2ef2c8fdded1b74ec283bba26e
Albert Peschar <albert@peschar.net>
2022-01-08 15:45:46 +0200

    Autoconfigure event subscribers and message handlers

--- plugin/src/Resources/config/services.php
+++ plugin/src/Resources/config/services.php
@@ -6,10 +6,9 @@ use Pricemotion\Shopware\Command\TestCommand;
 use Pricemotion\Shopware\Controller\ApiController;
 use Pricemotion\Shopware\Extension\Content\Product\PricemotionProductDefinition;
 use Pricemotion\Shopware\Extension\Content\Product\PricemotionProductExtension;
-use Pricemotion\Shopware\MessageQueue\Handler\ConfigureWebhookHandler;
-use Pricemotion\Shopware\Subscriber\ConfigSubscriber;
-use Pricemotion\Shopware\Subscriber\ProductWriteSubscriber;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 return function (ContainerConfigurator $configurator): void {
     $services = $configurator
@@ -18,15 +17,13 @@ return function (ContainerConfigurator $configurator): void {
         ->tag('monolog.logger', ['channel' => 'pricemotion'])
         ->autowire();
 
+    $services->instanceof(EventSubscriberInterface::class)->tag('kernel.event_subscriber');
+    $services->instanceof(MessageHandlerInterface::class)->tag('messenger.message_handler');
+
     $services->load('Pricemotion\\Shopware\\', dirname(__DIR__, 2) . '/*');
 
     $services->set(PricemotionProductExtension::class)->tag('shopware.entity.extension');
     $services->set(PricemotionProductDefinition::class)->tag('shopware.entity.definition');
-
-    $services->set(ConfigSubscriber::class)->tag('kernel.event_subscriber');
-    $services->set(ProductWriteSubscriber::class)->tag('kernel.event_subscriber');
-
-    $services->set(ConfigureWebhookHandler::class)->tag('messenger.message_handler');
 
     $services->set(ApiController::class)->public();
 
