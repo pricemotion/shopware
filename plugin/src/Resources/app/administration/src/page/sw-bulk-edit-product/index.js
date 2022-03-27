95fb9563588563c3c7a4eb4b17e21d14b8ea0cbd
Albert Peschar <albert@peschar.net>
2022-03-27 15:47:11 +0300

    Add form field

--- plugin/src/Resources/app/administration/src/page/sw-bulk-edit-product/index.js
+++ plugin/src/Resources/app/administration/src/page/sw-bulk-edit-product/index.js
@@ -1,3 +1,28 @@
 import template from './sw-bulk-edit-product.html.twig';
 
-Shopware.Component.override('sw-bulk-edit-product', { template });
+const FORM_PRICEMOTION = 'pricemotion';
+
+Shopware.Component.override('sw-bulk-edit-product', {
+  template,
+  computed: {
+    pricemotionFormFields() {
+      console.log('pricemotionFormFields');
+      return [
+        {
+          name: FORM_PRICEMOTION,
+          config: {
+            allowOverwrite: true,
+            allowClear: true,
+            changeLabel: this.$tc('pricemotion.bulkChangeLabel'),
+          },
+        },
+      ];
+    },
+  },
+  methods: {
+    loadBulkEditData() {
+      this.$super('loadBulkEditData');
+      this.defineBulkEditData(FORM_PRICEMOTION);
+    },
+  },
+});
