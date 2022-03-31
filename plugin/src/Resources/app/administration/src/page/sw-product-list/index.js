2992a07d3dd17048fbe0567db4aaf9a94019180f
Albert Peschar <albert@peschar.net>
2022-03-31 17:09:11 +0300

    Indicate price rule in product listing

--- plugin/src/Resources/app/administration/src/page/sw-product-list/index.js
+++ plugin/src/Resources/app/administration/src/page/sw-product-list/index.js
@@ -11,6 +11,12 @@ Component.override('sw-product-list', {
       result = [
         ...result,
         {
+          property: 'pricemotionHasPriceRule',
+          label: this.$tc('pricemotion.columnPriceRule'),
+          allowResize: true,
+          visible: false,
+        },
+        {
           property: 'extensions.pricemotion.lowestPrice',
           label: this.$tc('pricemotion.columnLowestPrice'),
           allowResize: true,
@@ -20,6 +26,11 @@ Component.override('sw-product-list', {
       ];
 
       return result;
+    },
+    hasPriceRule(product) {
+      const rule = product.extensions.pricemotion?.settings?.rule;
+      console.log(rule);
+      return rule && rule !== 'disabled';
     },
   },
 });
