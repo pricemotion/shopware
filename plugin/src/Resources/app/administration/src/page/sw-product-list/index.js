3f2283c5d9a66adba57bcbf3b2015da46ca20fe4
Albert Peschar <albert@peschar.net>
2022-03-31 15:20:20 +0300

    Format lowest price column

--- plugin/src/Resources/app/administration/src/page/sw-product-list/index.js
+++ plugin/src/Resources/app/administration/src/page/sw-product-list/index.js
@@ -1,6 +1,9 @@
 const { Component } = Shopware;
 
+import template from './sw-product-list.html.twig';
+
 Component.override('sw-product-list', {
+  template,
   methods: {
     getProductColumns() {
       let result = this.$super('getProductColumns');
@@ -8,7 +11,7 @@ Component.override('sw-product-list', {
       result = [
         ...result,
         {
-          property: 'pricemotionLowestPrice',
+          property: 'extensions.pricemotion.lowestPrice',
           label: this.$tc('pricemotion.columnLowestPrice'),
           allowResize: true,
           visible: false,
